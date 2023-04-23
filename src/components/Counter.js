493e5ff7529cce9bf2639e41e770af5f7f671512
gich2009 <waneneamos@gmail.com>
2023-04-24 00:29:44 +0300

    Completed assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,9 +4,10 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    //Changed the statement below to the recommended react syntax that uses the arrow function.
+    setCount((count) => count + 1);
   }
-
+  
   return <button onClick={increment}>I have been clicked {count} times</button>;
 }
 
